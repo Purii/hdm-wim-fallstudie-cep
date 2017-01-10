bcba004aa4754810ead6eaf05624e159c3b91280
Patrick Puritscher <blackout289@gmail.com>
2017-01-10 14:48:16 +0100

    restore ComplexToken and Event

---
+++ drools/src/main/java/de/hdm/wim/ComplexToken.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package de.hdm.wim;
+
+import java.util.ArrayList;
+
+/**
+ * @author Patrick Puritscher
+ *
+ */
+public class ComplexToken {
+
+	public static final String PROJECTDOCUMENTS = "projectdocuments";
+	
+	private String restrictedToProject;
+	private String restrictedToTime;
+	private ArrayList<String> topics;
+
+	/**
+	 * @return the topic
+	 */
+	public ArrayList<String> getTopics() {
+		return topics;
+	}
+	/**
+	 * @param topic the topic to set
+	 */
+	public void addTopic(String topic) {
+		this.topics.add(topic);
+	}
+
+	/**
+	 * @return the restrictedToProject
+	 */
+	public String getRestrictedToProject() {
+		return restrictedToProject;
+	}
+	/**
+	 * @param restrictedToProject the restrictedToProject to set
+	 */
+	public void setRestrictedToProject(String restrictedToProject) {
+		this.restrictedToProject = restrictedToProject;
+	}
+
+	/**
+	 * @return the restrictedToTime
+	 */
+	public String getRestrictedToTime() {
+		return restrictedToTime;
+	}
+	/**
+	 * @param restrictedToTime the restrictedToTime to set
+	 */
+	public void setRestrictedToTime(String restrictedToTime) {
+		this.restrictedToTime = restrictedToTime;
+	}
+	
+	public String toString() {
+		return "Get " + this.getTopics().size() + " Topics restricted to project " + this.getRestrictedToProject();
+	}
+}
